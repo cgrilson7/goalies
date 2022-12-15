0592df745549fe959d2c7d11dd8a928d8e503ee4
Colin G. Wilson <colingwilson7@gmail.com>
2022-12-14 23:26:11 -0500

    update, redeploy

--- goalies.R
+++ goalies.R
@@ -3,8 +3,8 @@
 library(readr)
 library(dplyr)
 
-goalies <- read_csv("https://moneypuck.com/moneypuck/playerData/seasonSummary/2022/regular/goalies.csv", col_names = FALSE) %>%
-  select(playerId = X1, season = X2, name = X3, team = X4, situation = X6, icetime = X8, xGoals = X9, goals = X10) %>%
+goalies <- read_csv("https://moneypuck.com/moneypuck/playerData/seasonSummary/2022/regular/goalies.csv") %>%
+  select(playerId, season, name, team, situation, icetime, xGoals, goals) %>%
   mutate(last_name = sub("^\\S+\\s+", '', name)) %>%
   mutate(last_name_plus_team = paste0(last_name, "\n", team)) %>%
   mutate(g60 = goals / (icetime/(60*60)),
@@ -38,11 +38,11 @@ ui <- fluidPage(
                   align = 'center',
            h1("Performance (GA/60) vs. Expected (xGA/60)"),
            h3("How to Read This Chart"),
-           h4("The vertical lines represent the average expected goals per 60 minutes for goalies shown, and +/- one standard deviation around this average."),
+           h4("The vertical lines represent the average expected goals per 60 minutes (for chosen situation), and +/- one standard deviation around this average."),
            h4("The diagonal lines represent various levels of goaltender performance vs. their expected goals against:"),
-           h4(strong("Purple - worse than expected", style = "color: #440154"), " (-1 GSAX per 60)", "|",
+           h4(strong("Purple - worse than expected", style = "color: #440154"), " (-1 s.d.)", "|",
               strong("Blue - performing as expected", style = "color: #31688e"), "|",
-              strong("Green - better than expected", style = "color: #35b779"), " (+1 GSAX per 60)"),
+              strong("Green - better than expected", style = "color: #35b779"), " (+1 s.d.)"),
            plotOutput('plot_diagonal', height = "800px", width = "100%"
            #            , hover = hoverOpts(
            #   id = 'plot_hover',
@@ -53,11 +53,11 @@ ui <- fluidPage(
            column(width = 6, align = 'center',
            h1("Goals Saved Above Expected (GSAX/60)"),
            h3("How to Read This Chart"),
-           h4("The vertical line represents the average expected goals per 60 minutes for goalies shown."),
+           h4("The vertical line represents the average expected goals per 60 minutes (for chosen situation)."),
            h4("The horizontal lines represent various levels of goaltender performance vs. their expected goals against:"),
-           h4(strong("Purple - worse than expected", style = "color: #440154"), " (-1 GSAX per 60)", "|",
+           h4(strong("Purple - worse than expected", style = "color: #440154"), " (-1 s.d.)", "|",
               strong("Blue - performing as expected", style = "color: #31688e"), "|",
-              strong("Green - better than expected", style = "color: #35b779"), " (+1 GSAX per 60)"),
+              strong("Green - better than expected", style = "color: #35b779"), " (+1 s.d.)"),
            plotOutput('plot_gsax', height = "800px", width = "100%"
                       #            , hover = hoverOpts(
                       #   id = 'plot_hover',
@@ -96,21 +96,23 @@ output$plot_diagonal <- renderPlot({
   goalies_situation_filtered <- goalies %>%
     dplyr::filter(situation == input$situation_filter)
   
+  g60_avg      <- sum(goalies_situation_filtered$goals) / (sum(goalies_situation_filtered$icetime)/(60*60))
+  xg60_avg     <- sum(goalies_situation_filtered$xGoals) / (sum(goalies_situation_filtered$icetime)/(60*60))
+  g60_sd       <- sd(goalies_situation_filtered$g60, na.rm = T)
+  xg60_sd      <- sd(goalies_situation_filtered$xg60, na.rm = T)
+  gsax60_avg   <- sum(goalies_situation_filtered$gsax) / (sum(goalies_situation_filtered$icetime)/(60*60))
+  gsax60_sd    <- sd(goalies_situation_filtered$gsax60, na.rm = T)
+  
   req(input$icetime_filter)
   
   goalies_ongoal_filtered <- goalies_situation_filtered %>%
     dplyr::filter(icetime > quantile(icetime, (input$icetime_filter/100)) | playerId %in% as.integer(input$player_highlight)) %>%
-      left_join((tibble(playerId = as.integer(input$player_highlight)) %>% mutate(fill = "#ab8d2c", color = "#ab8d2c", fontface = "bold.italic")), by = c("playerId" = "playerId")) %>%
+      left_join((tibble(playerId = as.integer(input$player_highlight)) %>% mutate(fill = "#ab8d2c", color = "#ab8d2c", fontface = "bold.italic", stroke = 2)), by = c("playerId" = "playerId")) %>%
       mutate(fill = coalesce(fill, "grey10"),
              color = coalesce(color, "grey10"),
-             fontface = coalesce(fontface, "plain"))
+             fontface = coalesce(fontface, "plain"),
+             stroke = coalesce(stroke, 1))
   
-  g60_avg   = sum(goalies_ongoal_filtered$goals) / (sum(goalies_ongoal_filtered$icetime)/(60*60))
-  xg60_avg  = sum(goalies_ongoal_filtered$xGoals) / (sum(goalies_ongoal_filtered$icetime)/(60*60))
-  g60_sd    = sd(goalies_ongoal_filtered$goals / (goalies_ongoal_filtered$icetime/(60*60)))
-  xg60_sd   = sd(goalies_ongoal_filtered$xGoals / (goalies_ongoal_filtered$icetime/(60*60)))
-  gsax60_avg   = sum(goalies_ongoal_filtered$goals) / (sum(goalies_ongoal_filtered$icetime)/(60*60))
-  gsax60_sd    = sd(goalies_ongoal_filtered$goals / (goalies_ongoal_filtered$icetime/(60*60)))
   xmin_calc =   floor(min(goalies_ongoal_filtered$xg60)*2)/2
   xmax_calc = ceiling(max(goalies_ongoal_filtered$xg60)*2)/2
   ymin_calc =    floor(min(goalies_ongoal_filtered$g60)*2)/2
@@ -126,37 +128,20 @@ output$plot_diagonal <- renderPlot({
                     ylim = c(ymin_calc, ymax_calc)) +
     scale_x_continuous(breaks = round(c(seq(xmin_calc, xmax_calc, 0.5), xg60_avg - xg60_sd, xg60_avg, xg60_avg + xg60_sd), 2)) +
     scale_y_continuous(breaks = seq(ymin_calc, ymax_calc, 0.5)) +
-    geom_abline(slope = 1, intercept = 1,  color = "#440154", size = 2, linetype = 'dashed', alpha = 0.7) +
+    geom_abline(slope = 1, intercept = gsax60_sd,  color = "#440154", size = 2, linetype = 'dashed', alpha = 0.7) +
     geom_abline(slope = 1, intercept = 0,  color = "#31688e", size = 2, alpha = 0.7) +
-    geom_abline(slope = 1, intercept = -1,  color = "#35b779", size = 2, linetype = 'dashed', alpha = 0.7) +
+    geom_abline(slope = 1, intercept = -gsax60_sd,  color = "#35b779", size = 2, linetype = 'dashed', alpha = 0.7) +
     geom_vline(xintercept = xg60_avg - xg60_sd, color = "grey70", size = 2, linetype = 'dashed', alpha = 0.5) +
     geom_vline(xintercept = xg60_avg, color = "grey60", size = 2, alpha = 0.75) +
     geom_vline(xintercept = xg60_avg + xg60_sd, color = "grey70", size = 2, linetype = 'dashed', alpha = 0.5) +
-    geom_point(aes(size = icetime/(60*60), fill = fill, color = color)) +
+    geom_point(aes(fill = fill, stroke = stroke), shape = 21, size = 5) +
     scale_fill_identity() +
     ggrepel::geom_text_repel(aes(label = last_name, color = color, fontface = fontface), size = 6, max.overlaps = 20) +
     scale_color_identity() +
-    # geom_text(aes(label = last_name), hjust = -0.15, vjust = 0.5, size = 6, color = "grey10") +
-    # geom_text(x = xmin_calc, y = ymin_calc, label = "(+) Defense, (+) Goaltending", color = "grey10", size = 6, hjust = 0) +
     # annotate("polygon",
     #          x = c(xmin_calc, xg60_avg, xg60_avg, xmin_calc),
     #          y = c(xmin_calc, xg60_avg, ymin_calc, ymin_calc),
     #          fill = "#1f9e89", alpha = 0.1) +
-    # # geom_text(x = xmin_calc, y = ymax_calc, label = "(+) Defense, (-) Goaltending",  color = "grey10", size = 6, hjust = 0) +
-    # annotate("polygon",
-    #          x = c(xmin_calc, xg60_avg, xg60_avg, xmin_calc),
-    #          y = c(ymax_calc, ymax_calc, xg60_avg, xmin_calc),
-    #          fill = "#ffdb8c", alpha = 0.1) +
-    # # geom_text(x = xmax_calc, y = ymax_calc, label = "(-) Defense, (-) Goaltending",   color = "grey10", size = 6, hjust = 1) +
-    # annotate("polygon",
-    #          x = c(xg60_avg, xmax_calc, xmax_calc, xg60_avg),
-    #          y = c(ymax_calc, ymax_calc, xmax_calc, xg60_avg),
-    #          fill = "#482878", alpha = 0.1) +
-    # # geom_text(x = xmax_calc, y = ymin_calc, label = "(-) Defense, (+) Goaltending",  color = "grey10", size = 6, hjust = 1) +
-    # annotate("polygon",
-    #          x = c(xg60_avg, xmax_calc, xmax_calc, xg60_avg),
-    #          y = c(xg60_avg, xmax_calc, ymin_calc, ymin_calc),
-    #          fill = "#31688e", alpha = 0.1) +
     labs(x = "Expected Goals Against per 60",
          y = "Goals Against per 60",
          size = "Total Icetime (60 Minute Increments)") +
@@ -183,21 +168,23 @@ output$plot_gsax <- renderPlot({
   goalies_situation_filtered <- goalies %>%
     dplyr::filter(situation == input$situation_filter)
   
+  g60_avg      <- sum(goalies_situation_filtered$goals) / (sum(goalies_situation_filtered$icetime)/(60*60))
+  xg60_avg     <- sum(goalies_situation_filtered$xGoals) / (sum(goalies_situation_filtered$icetime)/(60*60))
+  g60_sd       <- sd(goalies_situation_filtered$g60, na.rm = T)
+  xg60_sd      <- sd(goalies_situation_filtered$xg60, na.rm = T)
+  gsax60_avg   <- sum(goalies_situation_filtered$gsax) / (sum(goalies_situation_filtered$icetime)/(60*60))
+  gsax60_sd    <- sd(goalies_situation_filtered$gsax60, na.rm = T)
+  
   req(input$icetime_filter)
   
   goalies_ongoal_filtered <- goalies_situation_filtered %>%
     dplyr::filter(icetime > quantile(icetime, (input$icetime_filter/100)) | playerId %in% as.integer(input$player_highlight)) %>%
-    left_join((tibble(playerId = as.integer(input$player_highlight)) %>% mutate(fill = "#ab8d2c", color = "#ab8d2c", fontface = "bold.italic")), by = c("playerId" = "playerId")) %>%
+    left_join((tibble(playerId = as.integer(input$player_highlight)) %>% mutate(fill = "#ab8d2c", color = "#ab8d2c", fontface = "bold.italic", stroke = 2)), by = c("playerId" = "playerId")) %>%
     mutate(fill = coalesce(fill, "grey10"),
            color = coalesce(color, "grey10"),
-           fontface = coalesce(fontface, "plain"))
+           fontface = coalesce(fontface, "plain"),
+           stroke = coalesce(stroke, 1))
   
-  g60_avg   = sum(goalies_ongoal_filtered$goals) / (sum(goalies_ongoal_filtered$icetime)/(60*60))
-  xg60_avg  = sum(goalies_ongoal_filtered$xGoals) / (sum(goalies_ongoal_filtered$icetime)/(60*60))
-  gsax60_avg  = sum(goalies_ongoal_filtered$gsax) / (sum(goalies_ongoal_filtered$icetime)/(60*60))
-  g60_sd    = sd(goalies_ongoal_filtered$goals / (goalies_ongoal_filtered$icetime/(60*60)))
-  xg60_sd   = sd(goalies_ongoal_filtered$xGoals / (goalies_ongoal_filtered$icetime/(60*60)))
-  gsax60_sd   = sd(goalies_ongoal_filtered$gsax / (goalies_ongoal_filtered$icetime/(60*60)))
   xmin_calc =   floor(min(goalies_ongoal_filtered$xg60)*2)/2
   xmax_calc = ceiling(max(goalies_ongoal_filtered$xg60)*2)/2
   ymin_calc =    floor(min(goalies_ongoal_filtered$gsax60)*2)/2
@@ -214,34 +201,17 @@ output$plot_gsax <- renderPlot({
     scale_x_continuous(breaks = seq(xmin_calc, xmax_calc, 0.5)) +
     scale_y_continuous(breaks = seq(ymin_calc, ymax_calc, 0.5)) +
     geom_vline(xintercept = xg60_avg, color = "grey60", size = 2, alpha = 0.75) +
-    geom_hline(yintercept = -1,  color = "#440154", size = 2, linetype = 'dashed', alpha = 0.7) +
+    geom_hline(yintercept = -gsax60_sd,  color = "#440154", size = 2, linetype = 'dashed', alpha = 0.7) +
     geom_hline(yintercept = 0,  color = "#31688e", size = 2, linetype = 'solid', alpha = 0.7) +
-    geom_hline(yintercept = 1,  color = "#35b779", size = 2, linetype = 'dashed', alpha = 0.7) +
-    geom_point(aes(size = icetime/(60*60), fill = fill, color = color)) +
+    geom_hline(yintercept = gsax60_sd,  color = "#35b779", size = 2, linetype = 'dashed', alpha = 0.7) +
+    geom_point(aes(fill = fill, stroke = stroke), shape = 21, size = 5) +
     scale_fill_identity() +
     ggrepel::geom_text_repel(aes(label = last_name, color = color, fontface = fontface), size = 6, max.overlaps = 20) +
     scale_color_identity() +
-    # geom_text(aes(label = last_name), hjust = -0.15, vjust = 0.5, size = 6, color = "grey10") +
-    # geom_text(x = xmin_calc, y = ymin_calc, label = "(+) Defense, (+) Goaltending", color = "grey10", size = 6, hjust = 0) +
     # annotate("polygon",
     #          x = c(xmin_calc, xg60_avg, xg60_avg, xmin_calc),
     #          y = c(xmin_calc, xg60_avg, ymin_calc, ymin_calc),
     #          fill = "#1f9e89", alpha = 0.1) +
-    # # geom_text(x = xmin_calc, y = ymax_calc, label = "(+) Defense, (-) Goaltending",  color = "grey10", size = 6, hjust = 0) +
-    # annotate("polygon",
-    #          x = c(xmin_calc, xg60_avg, xg60_avg, xmin_calc),
-    #          y = c(ymax_calc, ymax_calc, xg60_avg, xmin_calc),
-    #          fill = "#ffdb8c", alpha = 0.1) +
-  # # geom_text(x = xmax_calc, y = ymax_calc, label = "(-) Defense, (-) Goaltending",   color = "grey10", size = 6, hjust = 1) +
-  # annotate("polygon",
-  #          x = c(xg60_avg, xmax_calc, xmax_calc, xg60_avg),
-  #          y = c(ymax_calc, ymax_calc, xmax_calc, xg60_avg),
-  #          fill = "#482878", alpha = 0.1) +
-  # # geom_text(x = xmax_calc, y = ymin_calc, label = "(-) Defense, (+) Goaltending",  color = "grey10", size = 6, hjust = 1) +
-  # annotate("polygon",
-  #          x = c(xg60_avg, xmax_calc, xmax_calc, xg60_avg),
-  #          y = c(xg60_avg, xmax_calc, ymin_calc, ymin_calc),
-  #          fill = "#31688e", alpha = 0.1) +
   labs(x = "Expected Goals Against per 60",
        y = "Goals Saved Above Expected per 60",
        size = "Total Icetime (60 Minute Increments)") +
